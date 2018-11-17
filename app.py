1043fb4805ee9d5300c82f5fb2237c13d745a34b
Yi Xie <yixie@u.boisestate.edu>
2018-11-16 17:28:36 -0700

    one hot encoding works

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from classify import oheClassify
 
 
 app = Flask(__name__)
-oheAccuracy = oheClassify()
+#oheAccuracy = oheClassify()
+w2vAccuracy = oheClassify()
 
 
 @app.route('/')
@@ -12,11 +13,11 @@ def index():
 
 @app.route('/w2v')
 def w2v():
-    return render_template('w2v.html')
+    return render_template('w2v.html', w2vscore=w2vAccuracy)
 
 @app.route('/ohe')
 def ohe():
-    return render_template('ohe.html', ohescore = oheAccuracy)
+    return render_template('ohe.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- classify.py
+++ classify.py
@@ -6,36 +6,19 @@ from sklearn import preprocessing
 from sklearn import linear_model
 from sklearn.metrics import accuracy_score
   
-# Creating array object 
 def oheClassify():
-    cols = ['polarity','id', 'date', 'query', 'user', 'tweet']
-    data = pd.read_csv('data/sentiment.csv',names=cols, encoding='ISO-8859-1')
-    #data = pd.read_pickle('data/sentiment.pkl')
-    data=data.sample(frac=0.1,random_state=200)
-    data = data.drop(['id', 'date', 'query', 'user'], axis=1)
-    data.polarity = data.polarity.apply(lambda x: 1 if x==4 else x)
-    # write the code here
-    # train take 80% of data
-    train = data.sample(frac=0.8, random_state=200)
-
-    #this code tries to find that train = 80% and the rest of data would be test and dev data tables which is test_dev = 20%
-    test_dev = data.loc[~data.index.isin(train.index)]
-
-    # for test_dev data set, split half-half 
-    #this code tries to find that test= 10% and dev = 10%
-    test = test_dev.sample(frac=0.5, random_state=200)
-    dev = test_dev.loc[~test_dev.index.isin(test.index),:]
-    train.shape, dev.shape, test.shape
-    # CountVectorizer is used in this case
-
-    vectorizer = CountVectorizer(lowercase=True)
-    X_train = vectorizer.fit_transform(train.tweet)
+    data = pd.read_pickle('data/sentiment.pkl')
+    train = data[:5000]
+    test =data[5000:]
+    cvX = CountVectorizer(token_pattern="\\w+", lowercase=True)
+    cvX.fit(data.tweet)
+    X_train = cvX.transform(train.tweet)
     y_train = train.polarity.as_matrix()
 
-    X_dev = vectorizer.transform(dev.tweet)
-    y_dev = dev.polarity.as_matrix()
+    X_test = cvX.transform(test.tweet)
+    y_test = test.polarity.as_matrix()
 
     model = linear_model.LogisticRegression(penalty='l2')
     model.fit(X_train, y_train)
-    accuracy = accuracy_score(model.predict(X_dev), y_dev) 
+    accuracy = accuracy_score(model.predict(X_test), y_test) 
     return accuracy
